570be38787add56f272fec4c7c2e0552ee60b975
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-02-10 14:47:22 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return <h1>App Component</h1>;
+  }
+}
+
+export default App;
