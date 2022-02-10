758f373294d79af604725a609e665943a1782d63
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-02-10 22:30:07 +0100

    added state variables that controls the active nav item selected

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
 import './App.css';
 import Footer from './components/Footer/Footer';
+import Navitem from './components/NavItem/Navitem';
+import { nanoid } from 'nanoid';
 
-class App extends React.Component {
-  constructor() {
-    super();
+function App() {
+  const [navItems, setNavItems] = React.useState([
+    { name: 'Profile', id: nanoid() },
+    { name: 'Education', id: nanoid() },
+    { name: 'Experience', id: nanoid() },
+    { name: 'Skills', id: nanoid() },
+  ]);
+  const [activeNavItem, setActiveNavItem] = React.useState('');
+  function setActive(id) {
+    setActiveNavItem(id);
   }
-  render() {
-    return (
-      <div className="flex-wrapper">
-        <header>My Cv Creator</header>
-        <aside>
-          <nav></nav>
-        </aside>
-        <main>
-          <form className="resume-form"></form>
-          <div className="preview-wrapper"></div>
-        </main>
-        <Footer />
-      </div>
-    );
-  }
+  const navList = navItems.map((item) => (
+    <Navitem
+      key={item.id}
+      name={item.name}
+      id={item.id}
+      active={activeNavItem}
+      setActive={setActive}
+    />
+  ));
+  console.log(activeNavItem);
+  return (
+    <div className="flex-wrapper">
+      <header>My Cv Creator</header>
+      <aside>
+        <nav>{navList}</nav>
+      </aside>
+      <main>
+        <form className="resume-form"></form>
+        <div className="preview-wrapper"></div>
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
