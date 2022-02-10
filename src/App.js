0b245dc796f4405c4f35d7789c143777265cc1ea
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-02-10 21:20:18 +0100

    updated Footer component path

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Footer from './components/Footer';
+import Footer from './components/Footer/Footer';
 
 class App extends React.Component {
   constructor() {
