985eb4c7af5f7c6a0323f12afdb023b67456ae3e
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-02-13 07:52:38 +0100

    added the button props

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
 function Button(props) {
+  const { name, id, handleClick, classification } = props;
   return (
-    <button onClick={(e) => props.handleClick(e, id)}>{props.name}</button>
+    <button
+      className={classification == 'Add' ? 'add' : 'delete'}
+      onClick={(e) => handleClick(e, id)}
+    >
+      {name}
+    </button>
   );
 }
 export default Button;
