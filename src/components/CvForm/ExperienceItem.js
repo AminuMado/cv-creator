843da08ebd2c0fb732b6e1dace8eef4d2efcae33
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-02-13 09:52:38 +0100

    added the experience items with their labels placeholders and value props

---
+++ src/components/CvForm/ExperienceItem.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import Input from '../Input/Input';
+
+function ExperienceItem(props) {
+  const { item, handleChange, id } = props;
+  return (
+    <section className="form-section">
+      <Input
+        name="companyName"
+        id={id}
+        label="Company Name"
+        placeholder="Google LLC"
+        value={item.companyName}
+        handleChange={handleChange}
+      />
+      <Input
+        name="jobTitle"
+        id={id}
+        label="Job Title"
+        placeholder="Software Engineer"
+        value={item.jobTitle}
+        handleChange={handleChange}
+      />
+      <Input
+        name="jobLocation"
+        id={id}
+        label="Job Location"
+        placeholder="Mountain View, CA"
+        value={item.jobLocation}
+        handleChange={handleChange}
+      />
+      <Input
+        name="startDate"
+        id={id}
+        label="Start Date"
+        placeholder="July 2021"
+        value={item.startDate}
+        handleChange={handleChange}
+      />
+      <Input
+        name="endDate"
+        id={id}
+        label="End Date"
+        placeholder="Present"
+        value={item.endDate}
+        handleChange={handleChange}
+      />
+      <Input
+        name="jobResponsibilites"
+        id={id}
+        label="Job Responsibilites"
+        placeholder="Did awesome stuff company"
+        value={item.jobResponsibilites}
+        handleChange={handleChange}
+      />
+    </section>
+  );
+}
+
+export default ExperienceItem;
