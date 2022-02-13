4b0dc0053e9c4194786939bd8966c200323a9470
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-02-13 08:16:11 +0100

    added a button type of button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './Button.css';
 
 function Button(props) {
   const { name, id, handleClick, classification } = props;
   return (
     <button
       className={classification == 'Add' ? 'add' : 'delete'}
+      type="Button"
       onClick={(e) => handleClick(e, id)}
     >
       {name}
