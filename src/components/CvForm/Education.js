a0d3ec1b4516de0b89f7d0ad59594d3435aa12f1
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-02-13 08:17:40 +0100

    imported a Button custom component and added it to the render function

--- src/components/CvForm/Education.js
+++ src/components/CvForm/Education.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import EducationItem from './EducationItem';
+import Button from '../Button/Button';
 
 function Education(props) {
-  const { formData, handleChange } = props;
+  const { formData, handleChange, handleAdd, handleDelete } = props;
   const EducationItems = formData.education.map((item) => {
     return (
       <EducationItem
@@ -14,6 +15,22 @@ function Education(props) {
     );
   });
 
-  return <>{EducationItems}</>;
+  return (
+    <>
+      {EducationItems}
+      <div className="buttons-container">
+        <Button
+          name="Add School"
+          handleClick={handleAdd}
+          classification="Add"
+        />
+        <Button
+          name="Delete School"
+          handleClick={handleDelete}
+          classification="Delete"
+        />
+      </div>
+    </>
+  );
 }
 export default Education;
