3784f4788569e07a99868a448747006185474611
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-02-13 09:51:36 +0100

    added the experence component and logic

---
+++ src/components/CvForm/Experience.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ExperienceItem from './ExperienceItem';
+import Button from '../Button/Button';
+
+function Experience(props) {
+  const { formData, handleChange, handleAdd, handleDelete } = props;
+  const ExperienceItems = formData.experience.map((item) => {
+    return (
+      <ExperienceItem
+        key={item.id}
+        id={item.id}
+        item={item}
+        handleChange={handleChange}
+      />
+    );
+  });
+
+  return (
+    <>
+      {ExperienceItems}
+      <div className="buttons-container">
+        <Button name="Add Job" handleClick={handleAdd} classification="Add" />
+        <Button
+          name="Remove Job"
+          handleClick={handleDelete}
+          classification="Delete"
+        />
+      </div>
+    </>
+  );
+}
+export default Experience;
