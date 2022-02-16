b681b05318e966eb6483b8dba1da79108d6dd537
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-02-16 20:17:57 +0100

    imported the cveducation compoenent

--- src/components/CvPreview/CvPreview.js
+++ src/components/CvPreview/CvPreview.js
@@ -3,12 +3,14 @@ import CvContact from './Contact';
 import CvHeader from './Header';
 import CvSkills from './Skills';
 import CvExperience from './Experience';
+import CvEducation from './Education';
 function CvPreview({ formData }) {
   return (
     <div className="cv-preview-wrapper">
       <CvHeader personalInfo={formData.personalInfo} />
       <CvContact personalInfo={formData.personalInfo} />
       <CvSkills skills={formData.skills} />
+      <CvEducation education={formData.education} />
       <CvExperience experience={formData.experience} />
     </div>
   );
