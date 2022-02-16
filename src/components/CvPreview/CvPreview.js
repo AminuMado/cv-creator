7db0307372d70fdf03047badaeeb514e1b4720ed
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-02-16 21:19:51 +0100

    added the cv profile component

--- src/components/CvPreview/CvPreview.js
+++ src/components/CvPreview/CvPreview.js
@@ -4,14 +4,20 @@ import CvHeader from './Header';
 import CvSkills from './Skills';
 import CvExperience from './Experience';
 import CvEducation from './Education';
+import CvProfile from './Profile';
 function CvPreview({ formData }) {
   return (
     <div className="cv-preview-wrapper">
       <CvHeader personalInfo={formData.personalInfo} />
-      <CvContact personalInfo={formData.personalInfo} />
-      <CvSkills skills={formData.skills} />
-      <CvEducation education={formData.education} />
-      <CvExperience experience={formData.experience} />
+      <div className="cv-left-column-wrapper">
+        <CvContact personalInfo={formData.personalInfo} />
+        <CvSkills skills={formData.skills} />
+        <CvEducation education={formData.education} />
+      </div>
+      <div className="cv-right-column-wrapper">
+        <CvProfile personalInfo={formData.personalInfo} />
+        <CvExperience experience={formData.experience} />
+      </div>
     </div>
   );
 }
