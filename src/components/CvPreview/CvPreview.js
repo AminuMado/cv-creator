ada70a1e0e6a749224848d0d38d85ed82fe2ecc7
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-02-16 19:55:30 +0100

    added the Experience component

--- src/components/CvPreview/CvPreview.js
+++ src/components/CvPreview/CvPreview.js
@@ -2,13 +2,14 @@ import React from 'react';
 import CvContact from './Contact';
 import CvHeader from './Header';
 import CvSkills from './Skills';
-
+import CvExperience from './Experience';
 function CvPreview({ formData }) {
   return (
     <div className="cv-preview-wrapper">
       <CvHeader personalInfo={formData.personalInfo} />
       <CvContact personalInfo={formData.personalInfo} />
       <CvSkills skills={formData.skills} />
+      <CvExperience experience={formData.experience} />
     </div>
   );
 }
