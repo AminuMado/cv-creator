ff050f8b3b749f1410917d979ce85f83167ce35a
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-02-16 19:57:49 +0100

    fixed the key attribute error

--- src/components/CvPreview/Skills.js
+++ src/components/CvPreview/Skills.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function CvSkills({ skills }) {
-  const skillsList = skills.map((skill) => <li>{skill}</li>);
+  const skillsList = skills.map((skill, index) => <li key={index}>{skill}</li>);
   return <div className="cv-skills-wrapper">{skillsList}</div>;
 }
 
