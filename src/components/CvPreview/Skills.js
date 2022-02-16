0ac84ae5feb0e99ef4428ed63ea66f348b349229
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-02-16 20:26:13 +0100

    added a h1 header for section

--- src/components/CvPreview/Skills.js
+++ src/components/CvPreview/Skills.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 function CvSkills({ skills }) {
   const skillsList = skills.map((skill, index) => <li key={index}>{skill}</li>);
-  return <div className="cv-skills-wrapper">{skillsList}</div>;
+  return (
+    <div className="cv-skills-wrapper">
+      <h1>Skills</h1>
+      {skillsList}
+    </div>
+  );
 }
 
 export default CvSkills;
