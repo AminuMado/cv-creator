ed6163353dd39d2c76a445553918728150fd32ee
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-02-17 22:08:41 +0100

    added more props from the form Data in the component

--- src/components/CvPreview/Header.js
+++ src/components/CvPreview/Header.js
@@ -1,12 +1,25 @@
 import React from 'react';
 
 function CvHeader({ personalInfo }) {
-  const { fullName, title } = personalInfo;
+  const { fullName, title, email, address, phoneNumber } = personalInfo;
 
   return (
     <div className="cv-header-wrapper">
-      <h1>{fullName}</h1>
-      <p>{title}</p>
+      <div class="flex-wrapper">
+        <div class="header-content-1">
+          <h1>{fullName}</h1>
+          <p>{title}</p>
+        </div>
+        <div class="header-content-2">
+          <div class="cv-contact-wrapper">
+            <h3>
+              <a href="#">{email}</a>
+            </h3>
+            <h3>{address}</h3>
+            <h3>{phoneNumber}</h3>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
